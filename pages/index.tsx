d2752d2a3a0269b1c83bb6cb2076c48a4febf929
Curest0x <43941751+curest0x1021@users.noreply.github.com>
2023-01-08 20:00:53 -0500

    Update home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,80 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
-import MyFooter from '../components/footer'
-
-const inter = Inter({ subsets: ['latin'] })
+import { Badge, Button, DarkThemeToggle, Dropdown, TextInput } from "flowbite-react";
+import { FiHelpCircle, FiSettings, FiSearch } from "react-icons/fi";
+import { HiOutlineLightningBolt } from "react-icons/hi";
+import MyFooter from "../components/footer";
+import { useState, KeyboardEvent } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 export default function Home() {
+  const [address, setAddress] = useState("");
+  const [error, setError] = useState("");
+  const [fromDate, setFromDate] = useState<Date>();
+  const [toDate, setToDate] = useState<Date>(new Date());
+
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      // @ts-ignore
+      const str: string = e.target.value;
+      if (str.match("[A-Za-z0-9]{44}")) {
+        setError("");
+        setAddress(str);
+      } else {
+        setError("Invalid address");
+      }
+    }
+  };
+
   return (
-    <div className="min-h-full">
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
+    <div className="h-screen flex flex-col bg-white dark:bg-gray-800 dark:text-white">
+      <div className="flex items-center justify-center py-4">
+        <span className="text-4xl font-bold mr-8 dark:text-white">DeBooks</span>
+        <Dropdown label={<FiSettings />} arrowIcon={false} color="gray" >
+          <Dropdown.Item>
+            <DarkThemeToggle />
+          </Dropdown.Item>
+          <Dropdown.Item>
+            <button
+              className="rounded-lg p-2.5 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
+              type="button"
+              aria-label="Toggle metadata"
             >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
+              <HiOutlineLightningBolt size={20} />
+            </button>
+          </Dropdown.Item>
+          <Dropdown.Item>
+            <button
+              className="rounded-lg p-2.5 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
+              type="button"
+              aria-label="Toggle metadata"
+            >
+              <FiHelpCircle size={20} />
+            </button>
+          </Dropdown.Item>
+        </Dropdown>
+      </div>
+      <main className="flex flex-col flex-1 items-center">
+        <div>
+          <TextInput
+            type="text"
+            placeholder="enter account address e.g. DeDao...uw2r"
+            className="w-96"
+            onKeyDown={handleKeyDown}
           />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
+          {error && <Badge color="failure" className="mt-2">{error}</Badge>}
         </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
+        <p className="mt-4 mb-2 text-xl font-semibold">Transaction Statement</p>
+        <div className="flex items-center">
+          <span className="mr-2">For the period</span>
+          <DatePicker selected={fromDate} onChange={(date: Date) => setFromDate(date)} className="block w-full border disabled:cursor-not-allowed disabled:opacity-50 bg-gray-50 border-gray-300 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 rounded-lg p-2.5 text-sm" />
+          <span className="mx-2">To</span>
+          <DatePicker selected={toDate} onChange={(date: Date) => setToDate(date)} className="block w-full border disabled:cursor-not-allowed disabled:opacity-50 bg-gray-50 border-gray-300 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 rounded-lg p-2.5 text-sm" />
+          <Button color="gray" className="ml-4" disabled={!(fromDate && toDate && (toDate.getTime() - fromDate.getTime() > 0))}>
+            <FiSearch />
+          </Button>
         </div>
       </main>
       <MyFooter />
     </div>
-  )
+  );
 }
